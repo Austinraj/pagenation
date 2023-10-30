a66edd17c740b4e40c48a272241cb25aace1b027
Austinraj <malaraustinraja25@gmail.com>
2023-10-30 08:42:08 +0530

    commit

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const Ele = document.createElement("a");
-Ele.innerHTML = "1";
-Ele.setAttribute("id", "page1");
-Ele.setAttribute("class", "page1");
 
 
-const nes = document.getElementById("page1");
+const bodyEl1 =document.createElement('div');
+bodyEl1.innerHTML = "";
+bodyEl1.setAttribute("id", "div1");
+bodyEl1.setAttribute("class", "container");
+
+const ele1 = document.body;
+ele1.appendChild(bodyEl1);
 
 
- 
- 
+const El2 = document.createElement('p');
+El2.innerHTML = "";
+El2.setAttribute("id", "pagenation");
+El2.setAttribute("class", "pragraph");
+
+const nes2 = document.querySelector("div#div1");
+nes2.appendChild(El2);
 
 
-
+const show = document.getElementById('pagenation');
 
 // this part is XMLHttpRequest code
 var http = new XMLHttpRequest();
@@ -20,9 +27,23 @@ http.onreadystatechange = function(){
  if(this.readyState ==4){
    if(this.status == 200){
         const id = JSON.parse(this.responseText);
+        let anws = "";
         id.forEach(({id , name, email}) => {
-              console.log(`id:${id} name:${name} email:${email}`);
+          //     anws = anws + `<p>Id:${id} Name:${name} Email:${email} `;
+
+              if(id <= 10){
+
+                
+                 anws = anws + `<p>Id:${id} Name:${name} Email:${email} `;
+
+              }
+               if(id <=20 && id >=11){
+                //  anws = anws + `<p>Id:${id} Name:${name} Email:${email} `;
+              }
         });
+        show.innerHTML = anws;
+
+
    }
    else{
     console.error("404 Not Found")
@@ -31,3 +52,45 @@ http.onreadystatechange = function(){
 }
 http.open("GET","https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json");
 http.send();
+
+
+
+ const bodyEl =document.createElement('div');
+ bodyEl.innerHTML = "";
+ bodyEl.setAttribute("id", "page");
+ bodyEl.setAttribute("class", "container1");
+
+ const ele = document.body;
+ ele.appendChild(bodyEl);
+
+const Ele = document.createElement('a');
+Ele.innerHTML = "Next";
+Ele.setAttribute("id", "page1");
+Ele.setAttribute("class", "a-link");
+Ele.setAttribute("href","#");
+
+
+const nes = document.querySelector("div#page");
+nes.appendChild(Ele);
+
+
+for(let i=1 ; i<=10; i++){
+    const Ele = document.createElement('a');
+    Ele.innerHTML = i;
+    Ele.setAttribute("class", "a-link");
+    Ele.setAttribute("href","#");
+     
+    
+    const nes = document.querySelector("div#page");
+nes.appendChild(Ele);
+}
+const Ele1 = document.createElement('a');
+
+Ele1.innerHTML = "pevs";
+Ele1.setAttribute("id", "page1");
+Ele1.setAttribute("class", "a-link");
+Ele1.setAttribute("href","#");
+
+nes.appendChild(Ele1);
+
+
